2b1ada87e0bd54a8294e961ce282308344b8a1dc
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2018-03-27 19:24:17 -0400

    go fmt

--- cmd/generate.go
+++ cmd/generate.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"path/filepath"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/flosch/pongo2"
 	"github.com/otiai10/copy"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
 var s *pongo2.TemplateSet
@@ -29,6 +30,20 @@ static directory into the output dir.`,
 			os.Exit(1)
 		}
 
+		var files []string
+		gatherErr := filepath.Walk(viper.GetString("directory.content"), func(path string, f os.FileInfo, err error) error {
+			if !f.IsDir() {
+				files = append(files, path)
+			}
+			return nil
+		})
+		if gatherErr != nil {
+			fmt.Printf("Got Error: %+v\n", gatherErr.Error())
+			os.Exit(1)
+		}
+
+		sort.Sort(fileList(files))
+
 		fs := pongo2.MustNewLocalFileSystemLoader("")
 		s = pongo2.NewSet("test set with base directory", fs)
 		s.Globals["base_directory"] = pwd
@@ -36,11 +51,40 @@ static directory into the output dir.`,
 			fmt.Printf("Got Error: %+v\n", initErr.Error())
 			os.Exit(1)
 		}
-		err := filepath.Walk(viper.GetString("directory.content"), visit)
-		if err != nil {
-			fmt.Printf("Got Error: %+v\n", err.Error())
-			os.Exit(1)
+
+		for _, file := range files {
+			fmt.Printf("Visiting: %s\n", file)
+			filePath := strings.TrimPrefix(file, viper.GetString("directory.content"))
+			context := pongo2.Context{
+				"dir":         filepath.Dir(filePath),
+				"path":        filePath,
+				"environment": viper.GetStringMap("environment"),
+			}
+			tpl, dtlErr := s.FromFile(file)
+			if dtlErr != nil {
+				fmt.Printf("Got Error: %+v\n", dtlErr.Error())
+				os.Exit(1)
+			}
+			tplOut, evalErr := tpl.ExecuteBytes(context)
+			if evalErr != nil {
+				fmt.Printf("Got Error: %+v\n", evalErr.Error())
+				os.Exit(1)
+			}
+			outPath := filepath.Join(viper.GetString("directory.output"), filePath)
+			os.MkdirAll(filepath.Dir(outPath), os.ModePerm)
+			fd, fileOpenErr := os.Create(outPath)
+			if fileOpenErr != nil {
+				fmt.Printf("Got Error: %+v\n", fileOpenErr.Error())
+				os.Exit(1)
+			}
+			defer fd.Close()
+			_, fileWriteErr := fd.Write(tplOut)
+			if fileWriteErr != nil {
+				fmt.Printf("Got Error: %+v\n", fileWriteErr.Error())
+				os.Exit(1)
+			}
 		}
+
 		copyErr := copy.Copy(viper.GetString("directory.static"), filepath.Join(viper.GetString("directory.output"), viper.GetString("directory.static")))
 		if copyErr != nil {
 			fmt.Printf("Got Error: %+v\n", copyErr.Error())
@@ -49,37 +93,23 @@ static directory into the output dir.`,
 	},
 }
 
-func visit(path string, f os.FileInfo, err error) error {
-	if !f.IsDir() {
-		fmt.Printf("Visiting: %s\n", path)
-		filePath := strings.TrimPrefix(path, viper.GetString("directory.content"))
-		context := pongo2.Context{
-			"path": filePath,
-			"environment": viper.GetStringMap("environment"),
-		}
-		tpl, dtlErr := s.FromFile(path)
-		if dtlErr != nil {
-			return dtlErr
-		}
-		tplOut, evalErr := tpl.ExecuteBytes(context)
-		if evalErr != nil {
-			return evalErr
-		}
-		outPath := filepath.Join(viper.GetString("directory.output"), filePath)
-		os.MkdirAll(filepath.Dir(outPath), os.ModePerm)
-		fd, fileOpenErr := os.Create(outPath)
-		if fileOpenErr != nil {
-			return fileOpenErr
-		}
-		defer fd.Close()
-		_, fileWriteErr := fd.Write(tplOut)
-		if fileWriteErr != nil {
-			return fileWriteErr
-		}
-	}
-	return nil
-}
-
 func init() {
 	rootCmd.AddCommand(generateCmd)
+}
+
+type fileList []string
+
+func (s fileList) Len() int {
+	return len(s)
+}
+func (s fileList) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s fileList) Less(i, j int) bool {
+	less := len(s[i]) < len(s[j])
+	if less {
+		return less
+	} else {
+		return strings.Compare(s[i], s[j]) < 0
+	}
 }
