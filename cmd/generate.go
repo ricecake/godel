5ab5c59edd46aadbb0310f13f414e59708233c8a
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2018-03-30 19:20:16 -0400

    make file sorting work right

--- cmd/generate.go
+++ cmd/generate.go
@@ -30,10 +30,10 @@ static directory into the output dir.`,
 			os.Exit(1)
 		}
 
-		var files []string
+		files := new(fileSet)
 		gatherErr := filepath.Walk(viper.GetString("directory.content"), func(path string, f os.FileInfo, err error) error {
 			if !f.IsDir() {
-				files = append(files, path)
+				files.addItem(path)
 			}
 			return nil
 		})
@@ -42,7 +42,7 @@ static directory into the output dir.`,
 			os.Exit(1)
 		}
 
-		sort.Sort(fileList(files))
+		files.sortFiles()
 
 		fs := pongo2.MustNewLocalFileSystemLoader("")
 		s = pongo2.NewSet("test set with base directory", fs)
@@ -52,32 +52,36 @@ static directory into the output dir.`,
 			os.Exit(1)
 		}
 
-		for _, file := range files {
-			fmt.Printf("Visiting: %s\n", file)
-			filePath := strings.TrimPrefix(file, viper.GetString("directory.content"))
+		for _, file := range files.Files {
+			fmt.Printf("Visiting: %s\n", file.path)
+
 			context := pongo2.Context{
-				"dir":         filepath.Dir(filePath),
-				"path":        filePath,
+				"dir":         file.dir,
+				"path":        file.contentPath,
 				"environment": viper.GetStringMap("environment"),
 			}
-			tpl, dtlErr := s.FromFile(file)
+
+			tpl, dtlErr := s.FromFile(file.path)
 			if dtlErr != nil {
 				fmt.Printf("Got Error: %+v\n", dtlErr.Error())
 				os.Exit(1)
 			}
+
 			tplOut, evalErr := tpl.ExecuteBytes(context)
 			if evalErr != nil {
 				fmt.Printf("Got Error: %+v\n", evalErr.Error())
 				os.Exit(1)
 			}
-			outPath := filepath.Join(viper.GetString("directory.output"), filePath)
-			os.MkdirAll(filepath.Dir(outPath), os.ModePerm)
-			fd, fileOpenErr := os.Create(outPath)
+
+			os.MkdirAll(filepath.Dir(file.outPath), os.ModePerm)
+
+			fd, fileOpenErr := os.Create(file.outPath)
 			if fileOpenErr != nil {
 				fmt.Printf("Got Error: %+v\n", fileOpenErr.Error())
 				os.Exit(1)
 			}
 			defer fd.Close()
+
 			_, fileWriteErr := fd.Write(tplOut)
 			if fileWriteErr != nil {
 				fmt.Printf("Got Error: %+v\n", fileWriteErr.Error())
@@ -97,19 +101,51 @@ func init() {
 	rootCmd.AddCommand(generateCmd)
 }
 
-type fileList []string
+type parsedFile struct {
+	path        string
+	name        string
+	contentPath string
+	outPath     string
+	dir         string
+	parts       []string
+}
 
-func (s fileList) Len() int {
-	return len(s)
+type fileSet struct {
+	Files []parsedFile
 }
-func (s fileList) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
+
+func (this *fileSet) addItem(path string) error {
+	contentPath := strings.TrimPrefix(path, viper.GetString("directory.content"))
+	outPath := filepath.Join(viper.GetString("directory.output"), contentPath)
+	dir, name := filepath.Split(contentPath)
+	parts := strings.Split(contentPath, string(filepath.Separator))
+	this.Files = append(this.Files, parsedFile{
+		path:        path,
+		name:        name,
+		contentPath: contentPath,
+		outPath:     outPath,
+		dir:         dir,
+		parts:       parts,
+	})
+	return nil
 }
-func (s fileList) Less(i, j int) bool {
-	less := len(s[i]) < len(s[j])
+
+func (this *fileSet) sortFiles() error {
+	sort.Sort(this)
+	return nil
+}
+
+func (s fileSet) Len() int {
+	return len(s.Files)
+}
+func (s fileSet) Swap(i, j int) {
+	s.Files[i], s.Files[j] = s.Files[j], s.Files[i]
+}
+func (s fileSet) Less(i, j int) bool {
+	less := len(s.Files[i].parts) < len(s.Files[j].parts)
 	if less {
 		return less
 	} else {
-		return strings.Compare(s[i], s[j]) < 0
+		return strings.Compare(s.Files[i].name, s.Files[j].name) < 0
 	}
 }
